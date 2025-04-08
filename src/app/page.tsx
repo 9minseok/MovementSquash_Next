7a552ddb01dfe7ed80f60f35c847a09c8e4b78f0
9minseok <minseok7746@naver.com>
2025-04-08 15:12:12 +0900

    add: shacdn ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,20 @@
+import { Button } from "@/components/ui/button";
+import { AuroraBackground } from "@/components/ui/aurora-background";
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center flex-col h-dvh text-8xl gap-5">
-      <div>
-        MOVEMENT
+    <AuroraBackground>
+      <div className="flex items-center justify-center flex-col h-dvh text-8xl gap-5 text-white">
+        <div>
+          MOVEMENT
+        </div>
+        <div>
+          SQUASH
+        </div>
+        <div>
+          <Button>Click me</Button>
+        </div>
       </div>
-      <div>
-        SQUASH
-      </div>
-    </div>
+    </AuroraBackground>
   );
 }
