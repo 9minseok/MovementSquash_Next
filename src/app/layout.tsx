d67aabe5e43cf5e7acdd32a6500cb2073626aab6
9minseok <minseok7746@naver.com>
2025-04-10 14:36:59 +0900

    build: edit error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-import PageTransition from "@/components/PageTranstion/PageTransition";
+// import PageTransition from "@/components/PageTranstion/PageTransition";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
