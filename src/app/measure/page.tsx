c6efdb834841e3d88fbc6cd38a1899cc640c4c75
9minseok <minseok7746@naver.com>
2025-04-10 19:27:36 +0900

    build: add Suspense

--- src/app/measure/page.tsx
+++ src/app/measure/page.tsx
@@ -1,29 +1,32 @@
 'use client';
 
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
 export default function Page() {
   const searchParams = useSearchParams();
   const item = searchParams.get("item");
 
   return (
-    <div
-      className="flex items-center flex-col justify-center h-dvh text-5xl bg-zinc-800"
-      style={{
-        backgroundImage: 'url("/images/squash_map.png")',
-        backgroundSize: '',
-        backgroundRepeat: 'no-repeat',
-        backgroundPosition: 'center',
-      }}
-    >
-      <div className="flex item-center flex-col absolute top-10 text-white">
-        <h1 className="text-3xl font-bold bg-opacity-60 p-2 rounded">
-          Measure Page
-        </h1>
-        <p className="text-xl bg-opacity-50 p-2 rounded">
-          선택한 메뉴: {item}
-        </p>
+    <Suspense>
+      <div
+        className="flex items-center flex-col justify-center h-dvh text-5xl bg-zinc-800"
+        style={{
+          backgroundImage: 'url("/images/squash_map.png")',
+          backgroundSize: '',
+          backgroundRepeat: 'no-repeat',
+          backgroundPosition: 'center',
+        }}
+      >
+        <div className="flex item-center flex-col absolute top-10 text-white">
+          <h1 className="text-3xl font-bold bg-opacity-60 p-2 rounded">
+            Measure Page
+          </h1>
+          <p className="text-xl bg-opacity-50 p-2 rounded">
+            선택한 메뉴: {item}
+          </p>
+        </div>
       </div>
-    </div>
+    </Suspense>
   );
 }
