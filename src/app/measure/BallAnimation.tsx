320ae139c00c765122065178186af998b0615e78
구민석 <minseok7746@naver.com>
2025-05-10 16:55:54 +0900

    build error

--- src/app/measure/BallAnimation.tsx
+++ src/app/measure/BallAnimation.tsx
@@ -2,7 +2,6 @@
 
 import { useEffect, useRef, useState } from 'react';
 import useMeasureStore from '@/stores/measureStore';
-import { off } from 'process';
 
 export const getSquashPositions = (count: number) => {
   const positionsByCount: Record<number, { top: string; left: string }[]> = {
